abe2ff37498b29dc966731ab2634b42facf2c780
Jun Shimura <junshimura@gmail.com>
2024-04-17 22:52:44 +0900

    Display locate

---
+++ Assets/Mover.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] float a = 1;
+    [SerializeField] float b = 0;
+    [SerializeField] float c = 1;
+    [SerializeField] float d = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            Vector3 pos = transform.position;
+            pos.x = a * pos.x + b;
+            pos.y = c * pos.y + d;
+            transform.position = pos;
+        }
+    }
+
+
+}
