3fb1550d0592fe308681fd998f2d8a1499a32f92
Jun Shimura <2120254+JunShimura@users.noreply.github.com>
2024-04-20 16:08:51 +0900

    MoverController

--- Assets/Mover.cs
+++ Assets/Mover.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class Mover : MonoBehaviour
 {
@@ -8,20 +9,25 @@ public class Mover : MonoBehaviour
     [SerializeField] float b = 0;
     [SerializeField] float c = 1;
     [SerializeField] float d = 0;
-    // Start is called before the first frame update
-    void Start()
-    {
+    [SerializeField] float e = 0;
+    [SerializeField] float f = 0;
 
-    }
+    [SerializeField] Transform[] targets;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0)) {
-            Vector3 pos = transform.position;
-            pos.x = a * pos.x + b;  //x'=ax+b
-            pos.y = c * pos.y + d;  //y'=cy+d
-            transform.position = pos;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("pressed");
+            foreach (Transform target in targets)
+            {
+                Vector3 pos = target.position;
+                pos.x = a * pos.x + b * pos.y + c;  //x'=ax+by+c
+                pos.y = d * pos.x + e * pos.y + f;  //y'=dx+ey+f
+                target.position = pos;
+            }
         }
     }
 
