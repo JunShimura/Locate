ddce291c827d6043e63c38b178ecd7f7349ba09f
Jun Shimura <j.shimura@sugawara.ac.jp>
2024-05-23 17:25:47 +0900

    渦巻き

--- Assets/CircleInstance.cs
+++ Assets/CircleInstance.cs
@@ -5,20 +5,22 @@ using UnityEngine;
 public class CircleInstance : MonoBehaviour
 {
     [SerializeField] GameObject target;
-    [SerializeField] uint divide = 10;
-    [SerializeField] uint r1 = 5;
-    [SerializeField] float r2 = 0;
-    [SerializeField] float r3 = 0;
+    [SerializeField][Header("•ªŠ„”")] uint divide = 10;
+    [SerializeField][Header("‰Šú‚Ì”¼Œa")] float r1 = 5;  //‰Šú‚Ì”¼Œa
+    [SerializeField][Header("ÅŒã‚Ì”¼Œa")] float r2 = 0;
+    [SerializeField][Header("ü‰ñ”")] float r3 = 0;
+    [SerializeField][Header("[‚³")] float depth = 1;
 
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i < divide*r3; i++)
+        for (int i = 0; i < divide * r3; i++)
         {
             float t = Mathf.PI * 2 * i / divide;
-            float posX = Mathf.Sin(t) * (r1 + r2 * i);
-            float posY = Mathf.Cos(t) * (r1 + r2 * i);
-            Vector3 pos = new Vector3(posX, posY, 0);
+            float posX = Mathf.Sin(t) * (r1 + r2 * i / (divide * r3));
+            float posY = Mathf.Cos(t) * (r1 + r2 * i / (divide * r3));
+            float posZ = depth * i / (divide * r3);
+            Vector3 pos = new Vector3(posX, posY, posZ);
             GameObject.Instantiate(target, pos, Quaternion.identity);
         }
 
